7eb54bb20b14a96d9a16322a2c04475746acc151
Ivanka-karaim <karaimivanna@gmail.com>
2023-05-10 12:38:20 +0300

    all edit

--- lib/features/session/bloc/session/session_bloc.dart
+++ lib/features/session/bloc/session/session_bloc.dart
@@ -35,47 +35,51 @@ class SessionBloc extends Bloc<SessionEvent, SessionState> {
   }
 
   void _buySeats(BuySeatsEvent event, Emitter<SessionState> emit) async {
-    final accessToken = await _tokenLocalDatasource.getToken();
-    if (accessToken == null) {
-      emit(SessionFailure(error: "Account error"));
-    } else {
-      RegExp reg = RegExp(r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$');
-
-      RegExp regCard = RegExp(r'^\d{4}(-?\d{4}){3}$');
-
-      RegExp regDate = RegExp(r'^\d{2}/\d{2}$');
-
-      RegExp regCvv = RegExp(r'^\d{3}$');
-
-      if (reg.hasMatch(event.email) == false) {
-        emit(BuyError(error: 'Неправильно введена пошта'));
-      } else if (regCard.hasMatch(event.cardNumber) == false) {
-        emit(BuyError(error: 'Неправильно введено номер карти'));
-      } else if (regDate.hasMatch(event.expirationDate) == false) {
-        emit(BuyError(error: 'Неправильно введено дату'));
-      } else if (regCvv.hasMatch(event.cvv) == false) {
-        emit(BuyError(error: 'Неправильно введено cvv'));
+    try {
+      final accessToken = await _tokenLocalDatasource.getToken();
+      if (accessToken == null) {
+        emit(SessionFailure(error: "Account error"));
       } else {
-        List<int> seats = [];
-        for (int i = 0; i < event.seats.length; i++) {
-          seats.add(event.seats[i].id);
-        }
+        RegExp reg = RegExp(r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$');
 
-        final buy = await _movieRepository.buySeats(
-            accessToken,
-            event.sessionId,
-            event.email,
-            event.cardNumber.replaceAll('-', ''),
-            event.expirationDate,
-            event.cvv,
-            seats);
+        RegExp regCard = RegExp(r'^\d{4}(-?\d{4}){3}$');
 
-        if (buy["success"] == false) {
-          emit(SessionFailure(error: buy["data"]));
+        RegExp regDate = RegExp(r'^\d{2}/\d{2}$');
+
+        RegExp regCvv = RegExp(r'^\d{3}$');
+
+        if (reg.hasMatch(event.email) == false) {
+          emit(BuyError(error: 'Неправильно введена пошта'));
+        } else if (regCard.hasMatch(event.cardNumber) == false) {
+          emit(BuyError(error: 'Неправильно введено номер карти'));
+        } else if (regDate.hasMatch(event.expirationDate) == false) {
+          emit(BuyError(error: 'Неправильно введено дату'));
+        } else if (regCvv.hasMatch(event.cvv) == false) {
+          emit(BuyError(error: 'Неправильно введено cvv'));
         } else {
-          emit(BuySuccessful());
+          List<int> seats = [];
+          for (int i = 0; i < event.seats.length; i++) {
+            seats.add(event.seats[i].id);
+          }
+
+          final buy = await _movieRepository.buySeats(
+              accessToken,
+              event.sessionId,
+              event.email,
+              event.cardNumber.replaceAll('-', ''),
+              event.expirationDate,
+              event.cvv,
+              seats);
+
+          if (buy["success"] == false) {
+            emit(SessionFailure(error: buy["data"]));
+          } else {
+            emit(BuySuccessful());
+          }
         }
       }
+    }catch (Exception ){
+      emit(SessionFailure(error: 'Уппссс, щось пішло не так'));
     }
   }
 
